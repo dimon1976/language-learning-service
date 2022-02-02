7d2f43cf40f854d20c9ced128ebe06cedfc427a7
Dimon <dima2000@tut.by>
2022-02-02 15:15:11 +0300

    add unversioned Files

---
+++ src/main/java/by/languagelearningservice/LanguageLearningServiceApplication.java
@@ -0,0 +1,13 @@
+package by.languagelearningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLearningServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageLearningServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/languagelearningservice/LanguageLearningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.languagelearningservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageLearningServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
