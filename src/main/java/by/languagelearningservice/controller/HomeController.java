3241f217e1bebcad3268811ea729d19b9df2a209
Dimon <dima2000@tut.by>
2022-02-20 12:41:24 +0300

    small fix add search in home

--- src/main/java/by/languagelearningservice/controller/HomeController.java
+++ src/main/java/by/languagelearningservice/controller/HomeController.java
@@ -1,11 +1,9 @@
 package by.languagelearningservice.controller;
 
-import by.languagelearningservice.entity.User;
 import by.languagelearningservice.entity.courses.Course;
 import by.languagelearningservice.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -14,11 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpSession;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -29,10 +22,18 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model,
-                       @PageableDefault(sort = {"courseId"}, direction = Sort.Direction.DESC) Pageable pageable,
-                       HttpSession httpSession) {
-        User user = (User) httpSession.getAttribute("user");
+                       @PageableDefault(sort = {"courseId"}, direction = Sort.Direction.DESC,size = 4) Pageable pageable) {
         Page<Course> courseList = courseService.getAllListCourse(pageable);
+        model.addAttribute("url", "/");
+        model.addAttribute("page", courseList);
+        return "index";
+    }
+
+    @GetMapping("search")
+    public String search(Model model,
+                         @RequestParam(value = "search", required = false) String search,
+                         @PageableDefault(sort = {"courseId"}, direction = Sort.Direction.DESC,size = 4) Pageable pageable) {
+        Page<Course> courseList = courseService.findAllByNameCourseLike(search, pageable);
         model.addAttribute("url", "/");
         model.addAttribute("page", courseList);
         return "index";
